ccd0f5cfcb8a8c0a7bb66930a3ba6679c92b4343
Donde <rasika-rajendra.donde@capgemini.com>
2019-03-05 10:42:21 +0530

    Added subtraction() function

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,8 @@ public class Calculator
 public static void addition(int number1, int number2){
                      Sysytem.out.println("Addition="(number1+number2));
    }
+
+public static void subtraction(int number1, int number2){
+                     Sysytem.out.println("Subtraction="(number1-number2));
+   }
 }
