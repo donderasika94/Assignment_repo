728c8eefffa8cbea105b344a8727c3a6325d73a6
Donde <rasika-rajendra.donde@capgemini.com>
2019-03-05 10:46:32 +0530

    Added multiplication() function

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,14 @@ public class Calculator
                     System.out.println("Hello World!!!");   
                     System.out.println("calculator java program");
     }
-public static void addition(int number1, int number2){
+    public static void addition(int number1, int number2){
                      Sysytem.out.println("Addition="(number1+number2));
    }
 
-public static void subtraction(int number1, int number2){
+    public static void subtraction(int number1, int number2){
                      Sysytem.out.println("Subtraction="(number1-number2));
+   }
+    public static void multiplication(int number1, int number2){
+                     Sysytem.out.println("multiplication="(number1*number2));
    }
 }
